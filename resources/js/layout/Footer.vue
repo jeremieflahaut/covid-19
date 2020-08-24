3499bcbbfdc63d5faaacc4cc49c97e30e0277f5d
jeremie <jeremieflahaut@gmail.com>
2020-08-24 15:33:44 +0200

    create example modal

--- resources/js/layout/Footer.vue
+++ resources/js/layout/Footer.vue
@@ -8,6 +8,18 @@
             </v-card-text>
             <v-divider class="my-0"></v-divider>
             <v-card-text class="primary lighten-1 white--text">
+                <div>
+                    <v-btn @click.stop="dialog = true" text x-small>Mentions légales</v-btn>
+                </div>
+                <v-dialog v-model="dialog" max-width="290">
+                    <v-card>
+                        <v-card-title class="headline">Mentions légales</v-card-title>
+                        <v-card-text>
+                            Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+                        </v-card-text>
+                    </v-card>
+                </v-dialog>
+
                 <strong>Jérémie Flahaut &copy; {{ new Date().getFullYear() }}</strong>
             </v-card-text>
         </v-card>
@@ -23,7 +35,8 @@ export default {
             btns: [
                 { name: 'linkedin', icon: 'fab fa-linkedin-in', href: 'https://www.linkedin.com/in/jeremie-flahaut', target: '_blank'},
                 { name: 'github', icon: 'fab fa-github', href: 'https://github.com/jeremieflahaut/covid-19', target: '_blank'},
-            ]
+            ],
+            dialog: false,
         }
     }
 }
